7b60c99603191edd5d5db60e03a898faaaeef06e
Gianfranco F H <gianfranco.fh@shopee.com>
2020-12-09 11:46:39 +0800

    ! Add new entites User and its migration

---
+++ src/entities/User.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column('varchar', { length: '200', unique: true })
+  username: string;
+
+  @Column('varchar', { length: '200' })
+  password: string;
+}

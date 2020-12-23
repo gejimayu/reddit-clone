9c7893c7072b0dcd0dd67327255327956e06cbb3
Gianfranco F H <gianfranco.fh@shopee.com>
2020-12-23 17:24:42 +0800

    Web: Add logout function

--- reddit-clone-web/src/components/Navbar/index.tsx
+++ reddit-clone-web/src/components/Navbar/index.tsx
@@ -1,19 +1,30 @@
 // Hooks
-import { useMeQuery } from '../../generated/graphql';
+import { useMeQuery, useLogoutMutation } from '../../generated/graphql';
 
 // Components
-import { Box, Flex, Link } from '@chakra-ui/react';
+import { Box, Flex, Link, Button } from '@chakra-ui/react';
 import NextLink from 'next/link';
 
 const Navbar: React.FC = () => {
   const { data } = useMeQuery();
+  const [logout, { client, loading: isLogoutLoading }] = useLogoutMutation();
+
   return (
     <Box>
       <Flex padding="15px" justifyContent="flex-end">
         {data?.me ? (
           <>
             <Box marginRight="10px">{data.me.username}</Box>
-            <Box>Logout</Box>
+            <Button
+              variant="link"
+              onClick={async () => {
+                await logout();
+                client.resetStore();
+              }}
+              isLoading={isLogoutLoading}
+            >
+              Logout
+            </Button>
           </>
         ) : (
           <>
--- reddit-clone-web/src/generated/graphql.tsx
+++ reddit-clone-web/src/generated/graphql.tsx
@@ -29,6 +29,7 @@ export type Mutation = {
   __typename?: 'Mutation';
   addUser?: Maybe<Response>;
   login?: Maybe<Response>;
+  logout?: Maybe<Scalars['Boolean']>;
   createPost?: Maybe<Post>;
   updatePost?: Maybe<Post>;
   deletePost?: Maybe<Scalars['Boolean']>;
@@ -111,6 +112,13 @@ export type LoginMutation = { __typename?: 'Mutation' } & {
   >;
 };
 
+export type LogoutMutationVariables = Exact<{ [key: string]: never }>;
+
+export type LogoutMutation = { __typename?: 'Mutation' } & Pick<
+  Mutation,
+  'logout'
+>;
+
 export type AddUserMutationVariables = Exact<{
   username: Scalars['String'];
   password: Scalars['String'];
@@ -192,6 +200,49 @@ export type LoginMutationResult = Apollo.MutationResult<LoginMutation>;
 export type LoginMutationOptions = Apollo.BaseMutationOptions<
   LoginMutation,
   LoginMutationVariables
+>;
+export const LogoutDocument = gql`
+  mutation Logout {
+    logout
+  }
+`;
+export type LogoutMutationFn = Apollo.MutationFunction<
+  LogoutMutation,
+  LogoutMutationVariables
+>;
+
+/**
+ * __useLogoutMutation__
+ *
+ * To run a mutation, you first call `useLogoutMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useLogoutMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [logoutMutation, { data, loading, error }] = useLogoutMutation({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useLogoutMutation(
+  baseOptions?: Apollo.MutationHookOptions<
+    LogoutMutation,
+    LogoutMutationVariables
+  >
+) {
+  return Apollo.useMutation<LogoutMutation, LogoutMutationVariables>(
+    LogoutDocument,
+    baseOptions
+  );
+}
+export type LogoutMutationHookResult = ReturnType<typeof useLogoutMutation>;
+export type LogoutMutationResult = Apollo.MutationResult<LogoutMutation>;
+export type LogoutMutationOptions = Apollo.BaseMutationOptions<
+  LogoutMutation,
+  LogoutMutationVariables
 >;
 export const AddUserDocument = gql`
   mutation AddUser($username: String!, $password: String!) {

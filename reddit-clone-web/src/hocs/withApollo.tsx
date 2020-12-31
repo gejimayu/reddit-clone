410da9e5b8effdac2aa97719a18efcef418bd693
Gianfranco F H <gianfranco.fh@shopee.com>
2020-12-31 12:59:48 +0800

    Web: Forward Cookie in SSR

--- reddit-clone-web/src/hocs/withApollo.tsx
+++ reddit-clone-web/src/hocs/withApollo.tsx
@@ -1,30 +1,38 @@
 import withApollo from '../libs/next-apollo';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { QueryPostsResponse } from '../generated/graphql';
+import { NextPageContext } from 'next';
 
-const apolloClient = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          posts: {
-            keyArgs: [],
-            merge(
-              existing: QueryPostsResponse | undefined,
-              incoming: QueryPostsResponse
-            ): QueryPostsResponse {
-              return {
-                ...incoming,
-                posts: [...(existing?.posts || []), ...(incoming.posts || [])],
-              };
+const apolloClient = (ctx?: NextPageContext) =>
+  new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+    headers: {
+      cookie: ctx?.req?.headers.cookie || '',
+    },
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            posts: {
+              keyArgs: [],
+              merge(
+                existing: QueryPostsResponse | undefined,
+                incoming: QueryPostsResponse
+              ): QueryPostsResponse {
+                return {
+                  ...incoming,
+                  posts: [
+                    ...(existing?.posts || []),
+                    ...(incoming.posts || []),
+                  ],
+                };
+              },
             },
           },
         },
       },
-    },
-  }),
-  credentials: 'include',
-});
+    }),
+    credentials: 'include',
+  });
 
 export default withApollo(apolloClient);
